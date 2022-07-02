9aab88a8578b64b24ff9a38f913c7ce0f4d3064a
Amit2678 <92388641+Amit2678@users.noreply.github.com>
2022-07-02 12:42:36 +0530

    movie tickit app

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Header from '../common/header/Header.js';
+import Home from '../screens/home/Home.js';
+
+ReactDOM.render(
+    <>
+        <Home />
+    </>,
+    document.getElementById('root')
+);
